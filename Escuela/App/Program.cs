a29f1862f3bf2781dfc894cabe21ce75268cf71a
Jose Jair <jjarosemena@gmail.com>
2021-11-27 13:53:18 -0500

    proyecto escuela curso C#

---
+++ Escuela/App/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using CoreEscuela.Entidades;
+using CoreEscuela.App;
+
+namespace CoreEscuela
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var engine = new EscuelaEngine();
+            engine.Inicializar();
+
+            Util.Printer.DigujarLinea();
+
+            Console.WriteLine(engine.Escuela );
+            engine.ImprimirCusosEscuela();
+
+           
+            Console.Read();
+        }
+
+      
+    }
+}
---
+++ Escuela/Util/Printer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace CoreEscuela.Util
+{
+    public static class Printer
+    {
+        public static void DigujarLinea(int tamaño = 20)
+        {
+            Console.WriteLine("".PadLeft(tamaño, '='));
+        }
+
+        public static void WriteTitle (string Titulo )
+        {
+            int largo = Titulo.Length + 4;
+            Printer.DigujarLinea(largo );
+            Console.WriteLine($"| {Titulo} |");
+            Printer.DigujarLinea(largo );
+            Beep();
+        }
+
+        public static void Beep(int hz=900, int tiempo =100, int cantidad = 1)
+        {
+            while (cantidad-- > 0 )
+            {
+                Console.Beep(hz, tiempo);
+               
+            }
+        }
+    }
+}
